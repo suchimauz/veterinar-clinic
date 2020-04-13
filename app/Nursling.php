952b57e643fd7cc3c198c74e3722e64aeb4242ea
suchimauz <alexey.fedulov98@gmail.com>
2020-04-13 10:30:50 +0300

    fix breed field name in Nursling model

--- app/Nursling.php
+++ app/Nursling.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Nursling extends Model
 {
-    protected $fillable = ['nickname', 'address', 'phone', 'bredd', 'owner_name', 'user_id', 'category_id', 'created_at', 'updated_at'];
+    protected $fillable = ['nickname', 'address', 'phone', 'breed', 'owner_name', 'user_id', 'category_id', 'created_at', 'updated_at'];
 }
